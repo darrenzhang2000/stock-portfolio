b79fd5e9590c222c29b5d870ffe20367588bbfb0
Darren Zhang <darrenzhang200027@gmail.com>
2020-05-22 12:55:51 -0400

    added register page

--- stock-portfolio-front-end/src/App.js
+++ stock-portfolio-front-end/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Login from './components/Login'
+import Register from './components/Register'
 
 function App() {
   return (
     <div className="App">
       Hello world
       <Login/>
+      <Register/>
     </div>
   );
 }
