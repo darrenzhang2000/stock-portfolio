23ae33f1e34a49ecbf09148b849a4e3ef85c9761
Darren Zhang <darrenzhang200027@gmail.com>
2020-05-22 12:48:38 -0400

    added login form component

--- stock-portfolio-front-end/src/App.js
+++ stock-portfolio-front-end/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
       Hello world
+      <Login/>
     </div>
   );
 }
