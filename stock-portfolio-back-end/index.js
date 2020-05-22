142ef29fff4ad6f47257abca9b53a10be16c9710
Darren Zhang <darrenzhang200027@gmail.com>
2020-05-22 19:24:09 -0400

    added register and login endpoints and post requests

--- stock-portfolio-back-end/index.js
+++ stock-portfolio-back-end/index.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const app = express()
 
+// parse application/x-www-form-encoded and application/json
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
+
 //models
 const User = require('./models/User')
 
--- stock-portfolio-back-end/routes/users.js
+++ stock-portfolio-back-end/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/User')
 
 router.get('/login', (req, res) => {
     res.send('login')
@@ -7,6 +8,26 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
     res.send('register')
+})
+
+router.post('/register', (req, res) => {
+    //create a user using the payload
+    const { name, email, password, balance } = req.body
+    const user = new User({
+        name: name,
+        email: email, 
+        password: password,
+        balance: balance
+    })
+
+    //add the user to the db
+    user.save(err => {
+        if(err){
+            console.log(err)
+        }else{
+            console.log('user successfully added: ', user)
+        }
+    })
 })
 
 module.exports = router
