2276660bc608c182cae5f5b9d94e2c51e2bf67da
Darren Zhang <darrenzhang200027@gmail.com>
2020-05-22 20:56:32 -0400

    added bcrypt to hash user passwords before storing into db

--- stock-portfolio-back-end/routes/users.js
+++ stock-portfolio-back-end/routes/users.js
@@ -1,32 +1,42 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-const User = require('../models/User')
+const User = require("../models/User")
 
-router.get('/login', (req, res) => {
-    res.send('login')
+//for hashing passwords
+const bcrypt = require("bcrypt")
+const saltRounds = 10
+
+router.get("/login", (req, res) => {
+    res.send("login")
 })
 
-router.get('/register', (req, res) => {
-    res.send('register')
+router.get("/register", (req, res) => {
+    res.send("register")
 })
 
-router.post('/register', (req, res) => {
-    //create a user using the payload
-    const { name, email, password, balance } = req.body
-    const user = new User({
-        name: name,
-        email: email, 
-        password: password,
-        balance: balance
-    })
+router.post("/register", (req, res) => {
+    var { name, email, password, balance } = req.body
 
-    //add the user to the db
-    user.save(err => {
-        if(err){
-            console.log(err)
-        }else{
-            console.log('user successfully added: ', user)
-        }
+    //hash password
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+        let hashedPassword = hash
+
+        //create a user using the payload and hashed password
+        const user = new User({
+            name: name,
+            email: email,
+            password: hashedPassword,
+            balance: balance,
+        })
+
+        //add the user to the db
+        user.save((err) => {
+            if (err) {
+                console.log(err)
+            } else {
+                // console.log('user successfully added: ', user)
+            }
+        })
     })
 })
 
