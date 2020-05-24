fda61476fc3e4835ab504a674f0c38a71e6b09ce
Darren Zhang <darrenzhang200027@gmail.com>
2020-05-24 08:45:36 -0400

    added react routing

--- stock-portfolio-front-end/src/App.js
+++ stock-portfolio-front-end/src/App.js
@@ -2,13 +2,36 @@ import React from 'react';
 import './App.css';
 import LoginContainer from './containers/Login'
 import RegisterContainer from './containers/Register'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      Hello world
-      <LoginContainer/>
-      <RegisterContainer/>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/containers/Login">Login</Link>
+              </li>
+              <li>
+                <Link to="/containers/Register">Register</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        {/* Looks through the child <Routes>s and returns the first one that matches the URL */}
+        <Switch>
+          <Route path="/containers/Login">
+            <LoginContainer/>
+          </Route>
+          <Route path="/containers/Register">
+            <RegisterContainer/>
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
