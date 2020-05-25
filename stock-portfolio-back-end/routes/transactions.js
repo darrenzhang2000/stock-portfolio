dfb05559e427046b4a660a3653333a1896c9a00d
Darren Zhang <darrenzhang200027@gmail.com>
2020-05-25 17:39:23 -0400

    added get route to retrieve a user's transaction history

--- stock-portfolio-back-end/routes/transactions.js
+++ stock-portfolio-back-end/routes/transactions.js
@@ -2,20 +2,31 @@ const express = require("express")
 const router = express.Router()
 const Transaction = require("../models/Transaction")
 
-router.get('/hello', (req, res) => res.send('hi'))
+//retrieve all of the user's transactions from the database
+router.get("/email/:email", (req, res) => {
+  var { email } = req.params
+
+  Transaction.find({ email: email }, (err, transactionHistory) => {
+    if (err) {
+      res.send({ success: false, transactionHistory })
+    } else {
+      res.send({ success: true, transactionHistory })
+    }
+  })
+})
 
 //add a user's transaction into the db
 router.post(
   "/email/:email/stock/:tickerSymb/qty/:qty/cost/:cost",
   async (req, res) => {
     var { email, tickerSymb, qty, cost } = req.params
-    console.log(req.params)
+
     //create transaction
     const transaction = new Transaction({
-      email: email,
-      tickerSymb: tickerSymb,
-      qty: qty,
-      cost: cost,
+      email,
+      tickerSymb,
+      qty,
+      cost,
     })
 
     //save transaction into database
