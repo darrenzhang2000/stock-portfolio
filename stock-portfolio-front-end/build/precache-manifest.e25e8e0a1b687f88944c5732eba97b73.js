faa596a6838abce7540c3fbb739136aaf8e6f99d
Darren Zhang <darrenzhang200027@gmail.com>
2020-05-31 23:14:20 -0400

    removed frontend build from git ignore

---
+++ stock-portfolio-front-end/build/precache-manifest.e25e8e0a1b687f88944c5732eba97b73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac6ea6360ce3f5a76ab1596ca69a53f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ce8d9c3b64d6ea5a6af",
+    "url": "/static/css/main.c0b267bf.chunk.css"
+  },
+  {
+    "revision": "12424f9757471648f415",
+    "url": "/static/js/2.073d8fbf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.073d8fbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ce8d9c3b64d6ea5a6af",
+    "url": "/static/js/main.05c3d547.chunk.js"
+  },
+  {
+    "revision": "7d5936adad73eb5a9739",
+    "url": "/static/js/runtime-main.7966f24c.js"
+  }
+]);
